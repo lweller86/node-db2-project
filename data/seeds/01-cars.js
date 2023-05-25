9a1df66a5f321bf82cca2d795a034ed74108330d
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-25 11:07:14 -0400

    Cars seed created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,28 @@
 // STRETCH
+const cars = [
+    {vin: '1111111111111', 
+    make: 'toyota',
+    model: 'tundra',
+    mileage: 2500,
+    title: 'Clean',
+    transmission: 'Manual' 
+    },
+    {vin: '1111111111111', 
+    make: 'toyota',
+    model: 'prius',
+    mileage: 25000,
+    title: 'Salvage',
+    transmission: 'automatic' 
+    },
+    {vin: '1111111111111', 
+    make: 'chevy',
+    model: 'silverado',
+    mileage: 250,
+    title: 'Clean',
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
