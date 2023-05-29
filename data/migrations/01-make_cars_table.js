da3a03a8d742873d92f04fc96ffb6ebb4ea4aa44
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-29 10:28:00 -0400

    Completed migrate up function

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,20 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+      tbl.string('vin', 17).notNullable().uniqe()
+
+      tbl.string('make', 128).notNullable()
+
+      tbl.string('model', 128).notNullable()
+
+      tbl.numeric('mileage', 128).unsigned().notNullable()
+
+      tbl.string('title', 128)
+
+      tbl.string('transmission', 128)
+    
+  })
 };
 
 exports.down = function (knex) {
