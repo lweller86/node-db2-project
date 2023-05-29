4fe88b1580ce75b3ff648be0ef1986c6e5cb34bb
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-29 11:07:35 -0400

    create and connect connect cars router to server

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.verb('/', async (req, res, next ) => {
+    res.json('getting all cars')
+})
+
+router.verb('/:id', async (req, res, next ) => {
+    res.json(`get cars by id${req.params.id}`)
+})
+
+router.verb('/', async (req, res, next ) => {
+    res.json('posting new cars.')
+})
+
+
+
+router.exports = router
